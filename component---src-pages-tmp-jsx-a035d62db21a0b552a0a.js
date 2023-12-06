1f074bce77b1b3e4d639dbee5bd45dadfa88b72b
riotamoriya <riotamoriya@gmail.com>
2023-12-06 12:16:15 +0900

    Updates

---
+++ component---src-pages-tmp-jsx-a035d62db21a0b552a0a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksample_page_get_visa=self.webpackChunksample_page_get_visa||[]).push([[641],{2217:function(e,a,t){t.r(a);var s=t(7294),l=(t(5362),t(5027)),n=(t(6936),t(7214),t(9132),t(4687)),c=(t(8956),t(1857),t(5794),t(9455)),d=t(9221);a.default=e=>{let{data:a}=e;const t=a.allMdx.edges;return s.createElement(l.Z,{disablePaddingBottom:!0},s.createElement("div",{className:d.mh},s.createElement(n.Z,{articles:c.explanations,mdx_nodes:t})))}}}]);
+//# sourceMappingURL=component---src-pages-tmp-jsx-a035d62db21a0b552a0a.js.map
--- component---src-pages-tmp-jsx-d2d52be9b185fbda1190.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunksample_page_get_visa=self.webpackChunksample_page_get_visa||[]).push([[641],{2217:function(e,a,t){t.r(a);var s=t(7294),l=(t(5362),t(5027)),n=(t(6936),t(7214),t(9132),t(4687)),c=(t(8956),t(1857),t(9559),t(9455)),d=t(9221);a.default=e=>{let{data:a}=e;const t=a.allMdx.edges;return s.createElement(l.Z,{disablePaddingBottom:!0},s.createElement("div",{className:d.mh},s.createElement(n.Z,{articles:c.s2,mdx_nodes:t})))}}}]);
-//# sourceMappingURL=component---src-pages-tmp-jsx-d2d52be9b185fbda1190.js.map
