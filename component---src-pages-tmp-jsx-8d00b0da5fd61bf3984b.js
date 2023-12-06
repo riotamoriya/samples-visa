51dab9474b2714127dfca638829e03243527dd43
riotamoriya <riotamoriya@gmail.com>
2023-12-06 16:49:25 +0900

    Updates

---
+++ component---src-pages-tmp-jsx-8d00b0da5fd61bf3984b.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknova_martia_official_visa_guide=self.webpackChunknova_martia_official_visa_guide||[]).push([[641],{2217:function(e,a,t){t.r(a);var i=t(7294),l=(t(5362),t(5027)),n=(t(6936),t(7214),t(9132),t(4687)),s=(t(8956),t(1857),t(9559),t(9455)),c=t(9221);a.default=e=>{let{data:a}=e;const t=a.allMdx.edges;return i.createElement(l.Z,{disablePaddingBottom:!0},i.createElement("div",{className:c.mh},i.createElement(n.Z,{articles:s.explanations,mdx_nodes:t})))}}}]);
+//# sourceMappingURL=component---src-pages-tmp-jsx-8d00b0da5fd61bf3984b.js.map
